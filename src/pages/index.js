04eb4af81b304582769a739d531832d9a1224107
Junior Alves <junioralves.webdeveloer@gmail.com>
2021-08-18 19:02:03 -0300

    Initial project

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Head>
+        <title>My Next Spinners</title>
+        <meta name="description" content="Spinner Galery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
