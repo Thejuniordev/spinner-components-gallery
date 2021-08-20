3f10eb808aabe5d77d538a55173af30f4454d884
Junior Alves <junioralves.webdeveloer@gmail.com>
2021-08-20 11:30:33 -0300

    Insert title

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,7 +4,7 @@ export default function Card() {
     return (
         <div className={styles.card}>
             <header className={styles.card__header}>
-
+                <strong className={styles.card__title}> Spinner 1</strong>
             </header>
 
             <button className={styles.card__btn}>
